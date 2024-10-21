ac2fcc5d18ffa5e1f7bf8468d883cb7e6b9a29f2
Syed Usama Shah <syedusamshah10@gmail.com>
2024-10-21 22:20:01 +0500

    Backend completed

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "../backend/routes/auth.routes.js";
+import messageRoutes from "../backend/routes/message.routes.js";
+import userRoutes from "../backend/routes/user.routes.js";
+import connectToMongodb from "./db/connectToMongoDb.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongodb();
+  console.log(`Server is running on port ${PORT}`);
+});
