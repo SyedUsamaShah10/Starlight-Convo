2c2d5a17add4b941d7d7281339b95f1e4d3b2b6b
Syed Usama Shah <syedusamshah10@gmail.com>
2024-10-23 16:15:49 +0500

    Applied Socket.io for online users and messages

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ import authRoutes from "../backend/routes/auth.routes.js";
 import messageRoutes from "../backend/routes/message.routes.js";
 import userRoutes from "../backend/routes/user.routes.js";
 import connectToMongodb from "./db/connectToMongoDb.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongodb();
   console.log(`Server is running on port ${PORT}`);
 });
